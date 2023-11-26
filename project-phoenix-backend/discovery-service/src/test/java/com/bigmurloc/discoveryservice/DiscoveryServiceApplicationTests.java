6c371cb33f35b72d94c40535737cbf2defb308d4
BigMurloc <dominik.pasymowski@gmail.com>
2023-11-26 10:07:05 +0100

    add discovery service

---
+++ project-phoenix-backend/discovery-service/src/test/java/com/bigmurloc/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigmurloc.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
